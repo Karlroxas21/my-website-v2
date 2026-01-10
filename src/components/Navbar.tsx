55107f0350f3c50836ad8c132621c12e993cf53e
Karl Marx Roxas <karlm.roxas@gmail.com>
2026-01-10 11:08:42 +0800

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,33 @@
+import { navIcons, navLinks } from "@constants/index"
+import dayjs from "dayjs";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div>
+                <img src="/public/images/logo.svg" />
+                <p className="font-bold">Karl's Website</p>
+
+                <ul>
+                    {navLinks.map(({ id, name }) => (
+                        <li key={id}>
+                            <p>{name}</p>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <ul>
+                    {navIcons.map(({ id, img }) => (
+                        <li key={id}>
+                            <img src={img} alt={`icon-${id}`} />
+                        </li>
+                    ))}
+                </ul>
+                <time>{dayjs().format('ddd MMM D h:mm A')}</time>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
